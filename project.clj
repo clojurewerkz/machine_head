ddb94c4d4dad3cde18b032cfe00a0e2a892a4799
Michael Klishin <michael@defprotocol.org>
2013-09-17 06:30:09 +0100

    Add Sonatype repos to get Clojure 1.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -13,7 +13,13 @@
   :aliases {"all" ["with-profile" "dev:dev,1.3:dev,1.4:dev,1.6:dev,master"]}
   :repositories {"eclipse-paho" {:url "https://repo.eclipse.org/content/groups/paho/"
                                  :snapshots false
-                                 :releases {:checksum :fail}}}
+                                 :releases {:checksum :fail}}
+                 "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
   :javac-options      ["-target" "1.6" "-source" "1.6"]
   :jvm-opts           ["-Dfile.encoding=utf-8"]
   :source-paths       ["src/clojure"]
