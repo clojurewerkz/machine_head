2be1dda126ad201051ae0034d3abded267528e0e
Michael Klishin <michael@defprotocol.org>
2013-09-17 06:30:22 +0100

    1.3 is not supported

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                    :plugins [[codox "0.6.4"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.3:dev,1.4:dev,1.6:dev,master"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.6:dev,master"]}
   :repositories {"eclipse-paho" {:url "https://repo.eclipse.org/content/groups/paho/"
                                  :snapshots false
                                  :releases {:checksum :fail}}
