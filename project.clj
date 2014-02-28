b599a3b67b11bd3bd88d332d99da061c1b987f24
Michael Klishin <michael@clojurewerkz.org>
2014-02-28 13:07:43 +0400

    Back to dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/machine_head "1.0.0-beta7"
+(defproject clojurewerkz/machine_head "1.0.0-beta8-SNAPSHOT"
   :description "Clojure MQTT client"
   :dependencies [[org.clojure/clojure          "1.5.1"]
                  [org.eclipse.paho/mqtt-client "0.4.0"]
