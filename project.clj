e8f7c677e2deb85cd13ba495e1b5e8e495cd1b12
Michael Klishin <michael@clojurewerkz.org>
2015-01-02 13:45:48 +0300

    Drop 1.5 profile

--- project.clj
+++ project.clj
@@ -3,14 +3,13 @@
   :dependencies [[org.clojure/clojure          "1.6.0"]
                  [org.eclipse.paho/mqtt-client "0.4.0"]
                  [clojurewerkz/support         "1.1.0"]]
-  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :plugins [[codox "0.8.10"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.7:dev,master"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.7:dev,master"]}
   :repositories {"eclipse-paho" {:url "https://repo.eclipse.org/content/repositories/paho-releases/"
                                  :snapshots false
                                  :releases {:checksum :fail}}
