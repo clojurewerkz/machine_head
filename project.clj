592549dc58aea4b6d17ee773a137f47e5f3e3dfe
Michael Klishin <michael@clojurewerkz.org>
2015-07-12 01:06:48 +0300

    Test against Clojure 1.7.0 GA

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure          "1.6.0"]
                  [org.eclipse.paho/mqtt-client "0.4.0"]
                  [clojurewerkz/support         "1.1.0"]]
-  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-RC2"]]}
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :plugins [[codox "0.8.10"]]
