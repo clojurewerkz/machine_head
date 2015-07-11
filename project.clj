eced76f137c87cea4ce34b9b65d62c7ba254557a
Michael Klishin <michael@clojurewerkz.org>
2015-07-12 01:15:28 +0300

    Clojure master is now 1.8.0-master-SNAPSHOT

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [org.eclipse.paho/mqtt-client "0.4.0"]
                  [clojurewerkz/support         "1.1.0"]]
   :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
+             :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :plugins [[codox "0.8.10"]]
                    :codox {:sources ["src/clojure"]
