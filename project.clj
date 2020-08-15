751b03b187e9cb9603a6c5ea79c80443d9a4ef24
TristeFigure <clement.malliet@kisskissbankbank.com>
2020-08-15 02:54:18 +0200

    New dependency: [clojurewerkz/propertied "1.3.0"]

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure          "1.8.0"]
                  [org.eclipse.paho/org.eclipse.paho.client.mqttv3 "1.2.5"]
                  [clojurewerkz/support         "1.5.0"]
+                 [clojurewerkz/propertied      "1.3.0"]]
   :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :master {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
